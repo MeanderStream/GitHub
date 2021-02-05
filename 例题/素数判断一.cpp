ea194a54d94447250e847f848bff03d9ca6a31f1
MeanderStream <3465319860@qq.com>
2021-02-05 12:58:44 +0800

    C_PlusPlus

---
+++ 例题/素数判断一.cpp
@@ -0,0 +1,28 @@
+//判断素数 是素数返回1，否则返回0
+int is_prime(int n) 
+
+	int j=0;
+
+	for (int m = 2; m <= n; m++)
+	{
+		if (n % m == 0)
+			j++;
+	}
+	if (j==1)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+
+
+int main() 
+{
+	for (int i = 2; i <= 200; i++)
+	{
+		if (is_prime(i) == 1)
+			printf("%d ", i);
+	}
+
+}
